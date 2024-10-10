3ca5729eea376e384a952c5eae73bef5a5cf3869
Afrie Irham <afrieirham.work@gmail.com>
2024-10-11 01:31:39 +0800

    add umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "@/trpc/react";
+import { env } from "@/env";
 
 export const metadata: Metadata = {
   title: "Kelas Mengaji Online",
@@ -29,6 +30,13 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
+      <head>
+        <script
+          defer
+          src="https://analytics.afrieirham.com/script.js"
+          data-website-id={env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+        />
+      </head>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
