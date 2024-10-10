f0ccec250e04e08affcb8058dff0bcf746b4c617
Afrie Irham <afrieirham.work@gmail.com>
2024-10-10 21:32:07 +0800

    WIP landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
+import Image from "next/image";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
-
   return (
     <HydrateClient>
-      <div>
-        <h1>Kelas Mengaji Online</h1>
-        <p>{hello.greeting}</p>
-      </div>
+      <main className="flex h-screen flex-col items-center justify-center gap-y-2 bg-emerald-100/30">
+        <p>Sedang dibina...</p>
+        <div className="flex items-center space-x-2">
+          <Image
+            src="/favicon.ico"
+            width={100}
+            height={100}
+            className="h-8 w-8"
+            alt="Logo Kelas Mengaji Online"
+          />
+          <h1 className="text-2xl font-bold">Kelas Mengaji Online</h1>
+        </div>
+        <p className="text-xl">Senarai kelas mengaji online di Malaysia.</p>
+      </main>
     </HydrateClient>
   );
 }
