757f56c707245a594cdf8f61220c376b655ca374
Afrie Irham <afrieirham.work@gmail.com>
2024-10-10 22:37:01 +0800

    add OG image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,19 @@ export const metadata: Metadata = {
   title: "Kelas Mengaji Online",
   description: "Senarai kelas mengaji online di Malaysia",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    type: "website",
+    url: "https://kelasmengaji.online",
+    title: "Kelas Mengaji Online",
+    description: "Senarai kelas mengaji online di Malaysia",
+    siteName: "Kelas Mengaji Online",
+    images: [{ url: "/og.png" }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@afrieirham_",
+    images: "/og.png",
+  },
 };
 
 export default function RootLayout({
