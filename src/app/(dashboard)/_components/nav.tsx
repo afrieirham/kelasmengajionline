7e5fa3570e27343de19f4c7c0f8a5763adeb7951
Afrie Irham <afrieirham.work@gmail.com>
2024-10-21 23:28:26 +0800

    add indicator in navbar

--- src/app/(dashboard)/_components/nav.tsx
+++ src/app/(dashboard)/_components/nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -9,43 +9,13 @@ import { PhoneIcon, PresentationIcon, UsersRoundIcon } from "lucide-react";
 
 import Logo from "./logo";
 
-const links = [
-  {
-    title: "Pengajar",
-    href: "/pengajar",
-    Icon: UsersRoundIcon,
-  },
-  {
-    title: "Kelas",
-    href: "/kelas",
-    Icon: PresentationIcon,
-  },
-  {
-    title: "Perhubungan",
-    href: "/perhubungan",
-    Icon: PhoneIcon,
-  },
-];
-
 function Nav({ type }: { type: "desktop" | "mobile" }) {
-  const pathname = usePathname();
-
   if (type === "mobile") {
     return (
       <nav className="grid gap-2 text-sm font-medium">
         <Logo />
         <div className="mt-4 flex flex-col space-y-2">
-          {links.map((link) => (
-            <Link
-              key={link.href}
-              href={link.href}
-              data-active={pathname.startsWith(link.href)}
-              className="flex items-center space-x-2 rounded-lg px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground data-[active=true]:bg-muted data-[active=true]:text-primary"
-            >
-              <link.Icon className="h-4 w-4" />
-              <span>{link.title}</span>
-            </Link>
-          ))}
+          <Links />
         </div>
       </nav>
     );
@@ -53,19 +23,91 @@ function Nav({ type }: { type: "desktop" | "mobile" }) {
 
   return (
     <nav className="grid items-start gap-2 px-2 text-sm font-medium sm:px-4">
-      {links.map((link) => (
-        <Link
-          key={link.href}
-          href={link.href}
-          data-active={pathname.startsWith(link.href)}
-          className="flex items-center space-x-2 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-gray-200 hover:text-primary data-[active=true]:bg-gray-200 data-[active=true]:text-primary"
-        >
-          <link.Icon className="h-4 w-4" />
-          <span>{link.title}</span>
-        </Link>
-      ))}
+      <Links />
     </nav>
   );
 }
 
 export default Nav;
+
+function Links() {
+  const pathname = usePathname();
+  const [links, setLinks] = useState([
+    {
+      title: "Pengajar",
+      href: "/pengajar",
+      Icon: UsersRoundIcon,
+      active: false,
+    },
+    {
+      title: "Kelas",
+      href: "/kelas",
+      Icon: PresentationIcon,
+      active: false,
+    },
+    {
+      title: "Perhubungan",
+      href: "/perhubungan",
+      Icon: PhoneIcon,
+      active: false,
+    },
+  ]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLinks((links) =>
+        links.map((link) =>
+          link.href === "/pengajar" ? { ...link, active: true } : link,
+        ),
+      );
+    }, 2000);
+    setTimeout(() => {
+      setLinks((links) =>
+        links.map((link) =>
+          link.href === "/kelas" || link.href === "/pengajar"
+            ? { ...link, active: true }
+            : link,
+        ),
+      );
+    }, 4000);
+    setTimeout(() => {
+      setLinks((links) =>
+        links.map((link) =>
+          link.href === "/kelas" ||
+          link.href === "/pengajar" ||
+          link.href === "/perhubungan"
+            ? { ...link, active: true }
+            : link,
+        ),
+      );
+    }, 6000);
+  }, []);
+
+  return (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          data-active={pathname.startsWith(link.href)}
+          className="flex items-center justify-between rounded-lg px-3 py-2 text-muted-foreground transition-all hover:bg-muted hover:text-primary data-[active=true]:bg-gray-200 data-[active=true]:text-primary"
+        >
+          <div className="flex items-center space-x-2">
+            <link.Icon className="h-4 w-4" />
+            <span>{link.title}</span>
+          </div>
+          <div className="relative h-2 w-2">
+            <div
+              data-active={link.active}
+              className="absolute h-full w-full animate-ping rounded-full bg-red-500 data-[active=true]:hidden"
+            />
+            <div
+              data-active={link.active}
+              className="absolute h-full w-full rounded-full data-[active=false]:bg-red-500 data-[active=true]:bg-emerald-500"
+            />
+          </div>
+        </Link>
+      ))}
+    </>
+  );
+}
