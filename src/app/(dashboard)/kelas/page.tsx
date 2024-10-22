21b3190513bf4e4d37ce3860223d53afa21f5ace
Afrie Irham <afrieirham.work@gmail.com>
2024-10-23 01:09:58 +0800

    update class info form

--- src/app/(dashboard)/kelas/page.tsx
+++ src/app/(dashboard)/kelas/page.tsx
@@ -13,7 +13,7 @@ function KelasPage() {
       <div className="mt-4 flex flex-col gap-6 rounded-t-lg border bg-white p-6 lg:flex-row">
         <div className="w-full space-y-6">
           {type === instructor.organization && (
-            <div className="grid w-full max-w-sm items-center gap-2">
+            <div className="grid w-full max-w-sm items-center gap-2 rounded-lg border p-4">
               <p className="text-sm font-medium">Pengajar terdiri daripada</p>
               {[{ title: "Guru lelaki" }, { title: "Guru perempuan" }].map(
                 (tag, i) => (
@@ -33,7 +33,7 @@ function KelasPage() {
               )}
             </div>
           )}
-          <div className="grid w-full max-w-sm items-center gap-2">
+          <div className="grid w-full max-w-sm items-center gap-2 rounded-lg border p-4">
             <p className="text-sm font-medium">Penyertaan Kelas</p>
             {[{ title: "Kelas individu" }, { title: "Kelas berkumpulan" }].map(
               (tag, i) => (
@@ -52,51 +52,57 @@ function KelasPage() {
               ),
             )}
           </div>
-          <div className="grid w-full max-w-sm items-center gap-2">
+          <div className="grid w-full max-w-sm items-center gap-2 rounded-lg border p-4">
             <p className="text-sm font-medium">Menerima Pelajar</p>
-            <p className="text-sm">Lelaki</p>
-            {[
-              { title: "Kanak-kanak (12 tahun dan ke bawah)" },
-              { title: "Remaja (13 tahun dan ke atas)" },
-              { title: "Dewasa (18 tahun dan ke atas)" },
-              { title: "Warga emas (60 tahun dan ke atas)" },
-            ].map((tag, i) => (
-              <div
-                key={String(`${tag.title}-${i}`)}
-                className="flex items-center gap-2"
-              >
-                <input type="checkbox" id={String(`${tag.title}-${i}`)} />
-                <Label
-                  htmlFor={String(`${tag.title}-${i}`)}
-                  className="font-normal"
-                >
-                  {tag.title}
-                </Label>
+            <div className="space-y-4">
+              <div className="flex flex-col gap-2">
+                <p className="text-xs font-medium underline">Lelaki</p>
+                {[
+                  { title: "Kanak-kanak (12 tahun dan ke bawah)" },
+                  { title: "Remaja (13 tahun dan ke atas)" },
+                  { title: "Dewasa (18 tahun dan ke atas)" },
+                  { title: "Warga emas (60 tahun dan ke atas)" },
+                ].map((tag, i) => (
+                  <div
+                    key={String(`l-${tag.title}-${i}`)}
+                    className="flex items-center gap-2"
+                  >
+                    <input type="checkbox" id={String(`l-${tag.title}-${i}`)} />
+                    <Label
+                      htmlFor={String(`l-${tag.title}-${i}`)}
+                      className="font-normal"
+                    >
+                      {tag.title}
+                    </Label>
+                  </div>
+                ))}
               </div>
-            ))}
-            <p className="mt-2 text-sm">Perempuan</p>
-            {[
-              { title: "Kanak-kanak (12 tahun dan ke bawah)" },
-              { title: "Remaja (13 tahun dan ke atas)" },
-              { title: "Dewasa (18 tahun dan ke atas)" },
-              { title: "Warga emas (60 tahun dan ke atas)" },
-            ].map((tag, i) => (
-              <div
-                key={String(`${tag.title}-${i}`)}
-                className="flex items-center gap-2"
-              >
-                <input type="checkbox" id={String(`${tag.title}-${i}`)} />
-                <Label
-                  htmlFor={String(`${tag.title}-${i}`)}
-                  className="font-normal"
-                >
-                  {tag.title}
-                </Label>
+              <div className="flex flex-col gap-2">
+                <p className="text-xs font-medium underline">Perempuan</p>
+                {[
+                  { title: "Kanak-kanak (12 tahun dan ke bawah)" },
+                  { title: "Remaja (13 tahun dan ke atas)" },
+                  { title: "Dewasa (18 tahun dan ke atas)" },
+                  { title: "Warga emas (60 tahun dan ke atas)" },
+                ].map((tag, i) => (
+                  <div
+                    key={String(`p-${tag.title}-${i}`)}
+                    className="flex items-center gap-2"
+                  >
+                    <input type="checkbox" id={String(`p-${tag.title}-${i}`)} />
+                    <Label
+                      htmlFor={String(`p-${tag.title}-${i}`)}
+                      className="font-normal"
+                    >
+                      {tag.title}
+                    </Label>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
 
-          <div className="grid w-full max-w-sm items-center gap-2">
+          <div className="grid w-full max-w-sm items-center gap-2 rounded-lg border p-4">
             <p className="text-sm font-medium">Format Kelas</p>
             {[
               { title: "Kelas online" },
@@ -117,32 +123,13 @@ function KelasPage() {
               </div>
             ))}
           </div>
-          <div className="grid w-full max-w-sm items-center gap-2">
+          <div className="grid w-full max-w-sm items-center gap-2 rounded-lg border p-4">
             <p className="text-sm font-medium">Yuran Kelas</p>
-            <p className="text-sm">Yuran Tetap</p>
             {[
-              { title: "Bulanan" },
-              { title: "Mengikut sesi" },
-              { title: "Mengikut umur pelajar" },
-            ].map((tag, i) => (
-              <div
-                key={String(`${tag.title}-${i}`)}
-                className="flex items-center gap-2"
-              >
-                <input type="checkbox" id={String(`${tag.title}-${i}`)} />
-                <Label
-                  htmlFor={String(`${tag.title}-${i}`)}
-                  className="font-normal"
-                >
-                  {tag.title}
-                </Label>
-              </div>
-            ))}
-            <p className="mt-2 text-sm">Yuran Flexible</p>
-            {[
-              { title: "Pelajar tentukan yuran" },
               { title: "Tiada yuran pendaftaran" },
+              { title: "Pelajar tentukan yuran" },
               { title: "Bayaran seikhlas hati" },
+              { title: "Kelas percuma" },
             ].map((tag, i) => (
               <div
                 key={String(`${tag.title}-${i}`)}
@@ -158,11 +145,13 @@ function KelasPage() {
               </div>
             ))}
           </div>
-          <div className="grid w-full max-w-sm items-center gap-2">
+
+          <div className="grid w-full max-w-sm items-center gap-2 rounded-lg border p-4">
             <p className="text-sm font-medium">Jadual Kelas</p>
             {[
               { title: "Hari/masa flexible" },
               { title: "Boleh ganti kelas" },
+              { title: "Pelajar tentukan jadual" },
             ].map((tag, i) => (
               <div
                 key={String(`${tag.title}-${i}`)}
@@ -178,7 +167,7 @@ function KelasPage() {
               </div>
             ))}
           </div>
-          <div className="grid w-full max-w-sm items-center gap-2">
+          <div className="grid w-full max-w-sm items-center gap-2 rounded-lg border p-4">
             <p className="text-sm font-medium">Pilih yang berkenaan</p>
             {[
               { title: "Kelas percubaan percuma" },
