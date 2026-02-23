7d1556add525ae546946d8f74b3a2fd46e2443f7
Afrie Irham <afrieirham.work@gmail.com>
2026-02-24 07:45:40 +0800

    housekeeping

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +8,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <p>hello world</p>;
 }
